b1671d3eb3d486fcfa35c8f4b971100a863089cc
TaniaG97 <gorkavik97@mail.ru>
2017-07-26 20:19:03 +0300

    Update InitBeacon.java

--- IndoorNavigation/beaconlibrary/src/main/java/ru/startandroid/beaconlibrary/InitBeacon.java
+++ IndoorNavigation/beaconlibrary/src/main/java/ru/startandroid/beaconlibrary/InitBeacon.java
@@ -1,9 +1,5 @@
 package ru.startandroid.beaconlibrary;
 
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-
 
 /**
  * Created by Admin on 24.07.2017.
