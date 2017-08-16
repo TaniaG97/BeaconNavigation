a4b0dfda75b54734334f2784f5320310f0cf0066
TaniaG97 <gorkavik97@mail.ru>
2017-08-16 23:50:14 +0300

    structure_changes

---
+++ IndoorNavigation/beaconlibrary/src/main/java/by/grsu/ftf/beaconlibrary/beacon/Beacon.java
@@ -0,0 +1,13 @@
+package by.grsu.ftf.beaconlibrary.beacon;
+
+/**
+ * Created by Admin on 16.08.2017.
+ */
+
+public class Beacon {
+    private static final String BEACON_TXT ="class Beacon";
+
+    public static String getBeaconTxt() {
+        return BEACON_TXT;
+    }
+}
